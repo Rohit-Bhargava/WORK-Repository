0d484825ea38f3cac6d50ad0cf6d56556a3d1e11
Rohit-Bhargava <Rohit_Bhargava_Freelance.outlook.com>
2021-07-21 20:41:45 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
-import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
