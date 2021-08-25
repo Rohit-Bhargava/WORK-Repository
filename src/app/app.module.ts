c4d07f0c252e019863ce142272ea60546e66cf16
Rohit-Bhargava <Rohit_Bhargava_Freelance.outlook.com>
2021-08-25 17:54:29 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
+
 import { SecondComponent } from './second/second.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent,
     SecondComponent
   ],
   imports: [
